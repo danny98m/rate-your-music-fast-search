d5de0cc8406de0d2d7817869c6db807392247530
Daniel Mullen <Danny@daniels-mbp.wheatonma.edu>
2018-09-04 11:05:50 -0400

    First commit

---
+++ rym.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+"""
+This program takes a command line arguments of a
+band/artist name and goes to their rym page.
+
+Later modify this for films and releases as well.
+But first i need to get unblocked from rym
+"""
+import sys, pyperclip, webbrowser
+
+def searchRym():
+	if len(sys.argv) > 1:
+		artist = '_'.join(sys.argv[1:])
+		artist = artist.lower()
+	else:
+		# if no arguments given just use the clipboard
+		artist = pyperclip.paste()
+		artist = artist.lower()
+
+	url = f'https://rateyourmusic.com/artist/{artist}'
+
+	webbrowser.open(url)
+
+
+def main():
+	searchRym()
+
+if __name__ == '__main__':
+	main()
